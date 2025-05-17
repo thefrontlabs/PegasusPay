3602fe7c6307635866fbdfeb9924cafe49712f81
priyanshu <priyanshoe.official@gmail.com>
2025-05-17 15:44:41 +0530

    commiting it

---
+++ src/data.js
@@ -0,0 +1,43 @@
+const data =  [
+  {
+    id: 1,
+    title: "Detailed achitecture declaration",
+    description: "Declare a Architrecture that can scale to diffrent tokens TRC20 and the native Trx.",
+    status: "completed",
+    date: "2025-05-17",
+    commitLink: ""
+  },
+  {
+    id: 2,
+    title: "Complete 'Gathere' Service",
+    description: "Gather the submition calls for crypto trasfers.",
+    status: "in-progress",
+    date: "2025-05-17",
+    commitLink: ""
+  },
+  {
+    id: 3,
+    title: "The WS service and Discovery service",
+    description: "The first point of contact for devs, to issue an payment sesion.",
+    status: "in-progress",
+    date: "2025-05-17",
+    commitLink: ""
+  },
+  {
+    id: 4,
+    title: "Scraper Service ",
+    description: "Scrape the complete transactrion to get hold of the crypto.",
+    status: "ideation",
+    date: "",
+    commitLink: ""
+  },
+  {
+    id: 5,
+    title: "Monitering",
+    description: "USing prometheus and Grafana to moniter the system stats.",
+    status: "ideation",
+    date: "",
+    commitLink: ""
+  }
+];
+export default data;
